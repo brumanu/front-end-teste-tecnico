366c65987046450e676182c131e6177e2ab0084c
brumanu <67878766+brumanu@users.noreply.github.com>
2025-01-20 14:44:11 -0300

    login and sign up styles

--- src/pages/login/CreateAccountView.vue
+++ src/pages/login/CreateAccountView.vue
@@ -1,78 +1,102 @@
 <template>
-    <PageWrapper>
-      <header>
-        <h1>Cadastro</h1>
-      </header>
-  
-      <div>
-        <label for="user">Usuario</label>
-  
-        <el-input
-          id="user"
-          v-model="payload.user"
-          placeholder="user"
-        />
-  
-        <label for="password">Senha</label>
-        <el-input
-          id="password"
-          v-model="payload.password"
-          type="password"
-          placeholder="Senha"
-          show-password
-        />
-        
-        <label for="confirm_password">Confirme a senha</label>
-        <el-input
-          id="confirm_password"
-          v-model="payload.confirm_password"
-          type="password"
-          placeholder="Confirme a senha"
-          show-password
-        />
-  
+  <PageWrapper style="width: 500px">
+    <header>
+      <h1>Cadastro</h1>
+    </header>
+
+    <div>
+      <label for="user">Usuario</label>
+
+      <el-input
+        id="user"
+        class="mb-1"
+        v-model="payload.user"
+        placeholder="Usuario"
+      />
+
+      <label for="password">Senha</label>
+      <el-input
+        id="password"
+        class="mb-1"
+        v-model="payload.password"
+        type="password"
+        placeholder="Senha"
+        show-password
+      />
+
+      <label for="confirm_password">Confirme a senha</label>
+      <el-input
+        class="mb-3"
+        id="confirm_password"
+        v-model="payload.confirm_password"
+        type="password"
+        placeholder="Confirme a senha"
+        show-password
+      />
+
+      <div class="flex flex-column">
         <el-button type="primary" @click="handleRegister">Cadastrar</el-button>
       </div>
-    </PageWrapper>
-  </template>
+    </div>
+  </PageWrapper>
+</template>
   
   <script setup>
-  import { ref } from 'vue';
-  import { useContasStore } from '@/stores/autenticacao'; 
-  import { useRouter } from 'vue-router';
-  import { useToast } from 'vue-toastification';
-  const toast = useToast();
+import { ref } from "vue";
+import { useContasStore } from "@/stores/autenticacao";
+import { useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
+const toast = useToast();
 
-  const router = useRouter();
-  
-  const payload = ref({
-    user: '',
-    password: '',
-    confirm_password: ''
-  });
-  
-  const contasStore = useContasStore();
-  
-  const handleRegister = () => {
-    if (!payload.value.user || !payload.value.password || !payload.value.confirm_password) {
-      toast.error('Todos os campos são obrigatórios');
-      return;
-    }
-  
-    if (payload.value.password !== payload.value.confirm_password) {
-      toast.error('As senhas não coincidem');
-      return;
-    }
-    const novoUsuario = {
-      usuario_id: String(contasStore.contas.length + 1),
-      nome: payload.value.user,
-      senha: payload.value.password
-    };
-  
-    contasStore.addConta(novoUsuario);
+const router = useRouter();
 
-    payload.value = {};
-    router.push('/');
+const payload = ref({
+  user: "",
+  password: "",
+  confirm_password: "",
+});
+
+const contasStore = useContasStore();
+
+const handleRegister = () => {
+  if (
+    !payload.value.user ||
+    !payload.value.password ||
+    !payload.value.confirm_password
+  ) {
+    toast.error("Todos os campos são obrigatórios");
+    return;
+  }
+
+  if (payload.value.password !== payload.value.confirm_password) {
+    toast.error("As senhas não coincidem");
+    return;
+  }
+  const novoUsuario = {
+    usuario_id: String(contasStore.contas.length + 1),
+    nome: payload.value.user,
+    senha: payload.value.password,
   };
-  </script>
+
+  contasStore.addConta(novoUsuario);
+
+  payload.value = {};
+  router.push("/");
+};
+</script>
+  <style lang="scss" scoped>
+header {
+  text-align: center;
+}
+
+#sign-up {
+  color: #409eff;
+  cursor: pointer;
+  transition: color 0.3s;
+
+  &:hover {
+    color: lighten(#409eff, 10%);
+  }
+}
+</style>
   
--- src/pages/login/LoginView.vue
+++ src/pages/login/LoginView.vue
@@ -1,66 +1,86 @@
 <template>
-  <PageWrapper>
+  <PageWrapper style="width: 500px">
     <header>
       <h1>Login</h1>
     </header>
 
-    <div>
+    <div class="mt-3">
       <label for="user">Usuario</label>
 
       <el-input
         id="user"
+        class="mb-1"
         v-model="payload.user"
         placeholder="Usuario"
       />
 
-      <label for="password">Senha</label>
+      <label class="mt-3" for="password">Senha</label>
       <el-input
         id="password"
+        class="mb-3"
         v-model="payload.password"
         type="password"
         placeholder="Senha"
         show-password
       />
 
-      <el-button type="primary" @click="handleLogin">Login</el-button>
+      <div class="flex flex-column">
+        <el-button type="primary" @click="handleLogin">Login</el-button>
 
-      <span @click="signUp()">Cadastre-se</span>
+        <span id="sign-up" @click="signUp()">Cadastre-se</span>
+      </div>
     </div>
   </PageWrapper>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useContasStore } from '@/stores/autenticacao';
-import { useRouter } from 'vue-router';
-import { useToast } from 'vue-toastification';
+import { ref } from "vue";
+import { useContasStore } from "@/stores/autenticacao";
+import { useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
 const toast = useToast();
 
 const router = useRouter();
 const payload = ref({
-  user: '',
-  password: ''
+  user: "",
+  password: "",
 });
 
 const contasStore = useContasStore();
 
 function signUp() {
-  router.push('/cadastro');
+  router.push("/cadastro");
 }
 
 const handleLogin = () => {
-    if (!payload.value.user || !payload.value.password) {
-        toast.error('Usuario e senha são obrigatórios');
-        return;
-    }
+  if (!payload.value.user || !payload.value.password) {
+    toast.error("Usuario e senha são obrigatórios");
+    return;
+  }
 
-    contasStore.login(payload.value.email, payload.value.password);
+  contasStore.login(payload.value.email, payload.value.password);
 
-    if (contasStore.isLoggedIn) {
-        // Redirecionar para a página inicial após o login bem-sucedido
-        router.push('/especialidades');
-    } else {
-        toast.error('Email ou senha incorretos');
-    }
+  if (contasStore.isLoggedIn) {
+    // Redirecionar para a página inicial após o login bem-sucedido
+    router.push("/especialidades");
+  } else {
+    toast.error("Email ou senha incorretos");
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+header {
+  text-align: center;
+}
+
+#sign-up {
+  color: #409eff;
+  cursor: pointer;
+  transition: color 0.3s;
+
+  &:hover {
+    color: lighten(#409eff, 10%);
+  }
+}
+</style>
